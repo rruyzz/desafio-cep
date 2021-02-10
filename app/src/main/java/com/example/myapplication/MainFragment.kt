37bec4a108c6f1dbc1f1ca128c62046a4912fa74
rruyzz <rodolfo.ruyz@gmail.com>
2021-02-10 19:58:51 -0300

    Mask Library

--- app/src/main/java/com/example/myapplication/MainFragment.kt
+++ app/src/main/java/com/example/myapplication/MainFragment.kt
@@ -27,48 +27,15 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
-        edit.addTextChangedListener(object : TextWatcher{
-            var isUpdating = false
-            override fun afterTextChanged(s: Editable?) {
-            }
-
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                if (isUpdating){
-                    isUpdating = false
-                    return
-                }
-                val hasMask = s.toString().indexOf('.') > -1 || s.toString().indexOf('-') > -1
-                var str = s.toString().filterNot{ it =='.' || it == '-'}
-                if (count> before){
-                    if(str.length > 5 ){
-                        str= "${str.substring(0,2)}.${str.substring(2,5)}-${str.substring(5)}"
-                    } else if (str.length>2){
-                        str= "${str.substring(0,2)}.${str.substring(2)}"
-                    }
-                    isUpdating = true
-                    edit.setText(str)
-                    edit.setSelection(edit.text?.length ?: 0)
-                } else {
-                    isUpdating = true
-                    edit.setText(str)
-                    edit.setSelection(Math.max(0,
-                        Math.min(if (hasMask) start - before else start, str.length)))
-                }
-            }
-        })
-
         btn.setOnClickListener {
-            var cep = edit.text.toString()
+            var cep = input.text.toString()
             cep = cep.replace(".", "")
             cep = cep.replace("-", "")
             var bundle = bundleOf("cep" to cep)
             NavHostFragment.findNavController(this)
                 .navigate(R.id.action_mainFragment_to_secondFragment, bundle)
         }
+
     }
 
 }
