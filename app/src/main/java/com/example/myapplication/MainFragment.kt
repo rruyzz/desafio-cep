6db6c2dc242e820a74e9c299b9a368f0d7abc194
rruyzz <rodolfo.ruyz@gmail.com>
2021-02-10 16:08:58 -0300

    first commit

---
+++ app/src/main/java/com/example/myapplication/MainFragment.kt
@@ -0,0 +1,28 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.NavHostFragment
+import kotlinx.android.synthetic.main.fragment_main.*
+
+class MainFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btn.setOnClickListener {
+            NavHostFragment.findNavController(this).navigate(R.id.action_mainFragment_to_secondFragment)
+        }
+    }
+}
